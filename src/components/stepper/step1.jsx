5a4b1078a3ea65fb8ad8bbbabc8fbd1d5e0cabd1
Saulo Silva <saulo@s10design.com.br>
2024-10-06 13:06:37 -0300

    add vercel

--- src/components/stepper/step1.jsx
+++ src/components/stepper/step1.jsx
@@ -5,8 +5,14 @@ function Step1() {
 
   return (
     <div className="border p-2">
-      <h1>Step 1 content: Create an account</h1>
-      <button type="button" className="rounded bg-blue-500 px-4 py-2 text-white" onClick={() => onStepClick(2)}>Não clica aqui</button>
+      <div className="divide-y divide-dashed">
+        <h1 className="p-2 font-bold">Step 1 content: Create an account</h1>
+        <h1 className="p-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor</h1>
+        <h1 className="p-2 text-red-600">lorem ipsum dolor sit amet. Donec auctor, nunc nec</h1>
+        <h1 className="p-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+      </div>
+
+      <button type="button" className="rounded bg-blue-500 px-4 py-2 text-white" onClick={() => onStepClick(1)}>Não clica aqui</button>
     </div>
   );
 }
