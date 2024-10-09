933acf3c67807c3a03d6cd0e175f082537234d94
Saulo Silva <saulo@s10design.com.br>
2024-10-08 22:24:41 -0300

    save code

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { StepperExample } from "./StepperExample.jsx";
 
 function App() {
   return (
-    <div className='flex h-screen flex-col items-center justify-center gap-4 bg-gray-50'>
+    <main role="main" className='flex h-screen flex-col items-center justify-center gap-4 bg-gray-50'>
       <StepperExample />
-    </div>
+    </main>
   )
 }
 
